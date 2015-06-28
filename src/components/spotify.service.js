86223024e7935dbffb46a21c8f9f9f29f98464a3
divyanshu1990 <mittal_divyanshu@yahoo.com>
2015-06-28 00:11:37 -0500

    new releases

--- src/components/spotify.service.js
+++ src/components/spotify.service.js
@@ -8,7 +8,8 @@
 
         var spotifyServiceFnVm = this;
         spotifyServiceFnVm.getMeTheSong = getMeTheSong;
-        spotifyServiceFnVm.token = "BQC-fm1BVaeybPhC51qJQ6NWg9LpxK8Ty7bThYjol6aO3redEWVLZixHTHJBM1EbpCA8awpgkkSS7ZPH0otoB_ja3qEuTe-vD7WpuW26zNvk0c8bvbm6aoe1zc3vrpCEnCa8RSg7GPs";
+        spotifyServiceFnVm.getNewReleases = getNewReleases;
+        spotifyServiceFnVm.token = "BQCk3saxkE05OAfGK0wJ8_9pTHfdCqzePONUUDsBzMg0Rf0-6c8Y0IeplOnPE7aJpXnlUhxaLp8OMCevzxR9UYph3RWInH5xIWWHdw5CQ9be3Y1GecOz2WBaKW9soN-ua3wk2hvNgOdj1ttggnzc_lMSavcSp4sVGseqnE3Kb90flBf4XzWa60Vts_kElWGx9QWctlVgYkyrYcjdo4PJq-nI_N-afqLT9gwFr1Q41TfcCUdn2XuGc2lVZ4JvFhLT42fJh6o3ZPbGYjWTwe2qrdGD1wGMjKXlQAG83iIbEJDiY1c";
 
         function getMeTheSong(query)  {
 
@@ -22,6 +23,27 @@
                 params: {
                     q: query,
                     type: 'track'
+                }
+            };
+
+            $http(request)
+                .success(function (result) {
+                    deferred.resolve(result);
+                }).error(function(error) {
+                    deferred.reject(error);
+                });
+
+            return deferred.promise;
+        }
+
+        function getNewReleases()  {
+
+            var deferred = $q.defer();
+            var request = {
+                url: 'https://api.spotify.com/v1/browse/new-releases',
+                method: 'GET',
+                headers: {
+                    Authorization: "Bearer " + spotifyServiceFnVm.token
                 }
             };
 
--- src/features/home/home.controller.js
+++ src/features/home/home.controller.js
@@ -2,9 +2,63 @@
     'use strict';
 
     angular.module('app').controller('HomeController', HomeControllerFn );
+    HomeControllerFn.$inject = ['spotifyService'];
 
-    function HomeControllerFn () {
+    function HomeControllerFn (spotifyService) {
         var homeVm = this;
+        homeVm.init = init;
+        homeVm.getNewSuccess = getNewSuccess;
+        homeVm.playSong = playSong;
+        homeVm.playSuccess = playSuccess;
+        homeVm.error = error;
+        homeVm.newReleases = [];
+        homeVm.index = 0;
+
+        init();
+        //////////////////////////
+        function init() {
+            spotifyService.getNewReleases().then(getNewSuccess, error);
+        }
+
+        function getNewSuccess(res) {
+            console.log(res);
+            homeVm.newReleases = res.albums.items;
+        }
+
+        function playSong(index, query) {
+            homeVm.index = index;
+            spotifyService.getMeTheSong(query).then(homeVm.playSuccess, homeVm.error);
+        }
+
+        function playSuccess(res) {
+
+            var player = $('#playMe');
+
+            if( homeVm.newReleases[homeVm.index].playing) {
+                player[0].pause();
+            }
+            else {
+                homeVm.url = res.tracks.items[0] ? res.tracks.items[0].preview_url : "";
+                player.attr('src', homeVm.url);
+                player[0].play();
+                player.bind('ended', function() {
+                    homeVm.newReleases[homeVm.index].playing = false;
+                    $scope.$apply();
+                });
+            }
+
+            (homeVm.newReleases).forEach(function(e, i){
+                if( i === homeVm.index )e.playing = !e.playing;
+                else e.playing = false;
+            });
+
+        }
+
+
+        function error() {
+            console.log('error')
+        }
+
     }
 
 })();
